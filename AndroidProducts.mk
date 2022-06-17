18e5f0b2b86dcd8df3edd22a9b4054066d41103d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-17 21:37:03 +0200

    ETW101GT_EEA: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: microtech Model: ETW101GT Script version: 2.3.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_ETW101GT_EEA.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_ETW101GT_EEA-user \
+    omni_ETW101GT_EEA-userdebug \
+    omni_ETW101GT_EEA-eng
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_ETW101GT_EEA-user
+add_lunch_combo omni_ETW101GT_EEA-userdebug
+add_lunch_combo omni_ETW101GT_EEA-eng
